2630dfa3e3274c1279f772258eb6963f5547eaf1
Nicolette Shaw <nikkikobako@gmail.com>
2020-10-31 10:04:33 +0000

    latest version of code with API

--- ajex-test/main.js
+++ ajex-test/main.js
@@ -1,20 +1,46 @@
-var xhr = new XMLHttpRequest();
-var data;
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
-xhr.open("GET","https://ci-swapi.herokuapp.com/api/");
-xhr.send();
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
 
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
 
-/* readystate of 4 means its done and the http status of 200 means ok
-once its ok we then get the xhr text 
-this however goes through a loop several times so the data we need wont be accessed 
-unless we do a timeout to let this function have time to run through all the states so it gets to 4*/
-xhr.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200 ){
-        data = JSON.parse(this.responseText);
-    }
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 }
 
-setTimeout(function() {
-    console.log(data);
-}, 500);
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`)
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+function writeToDocument(type) {
+    var tableRows = [];
+    var el = document.getElementById("data");
+
+    getData(type, function(data) {
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item) {
+            var dataRow = [];
+            Object.keys(item).forEach(function(key) {
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15);
+                dataRow.push(`<td>${truncatedData}</td>`);
+            });
+            tableRows.push(`<tr>${dataRow}</tr>`)
+        });
+
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+    });
+}
